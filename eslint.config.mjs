800b9f9b23a84bd29f20e75d744e6b2ae5587dc7
Johnny Estilles <johnny@estilles.com>
2026-05-08 21:09:24 +0800

    bump eslint from 5.13.0 to 10.3.0

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+})
+
+export default defineConfig([
+	{
+		ignores: ['node_modules/', 'eslint.config.mjs'],
+		extends: compat.extends('eslint:recommended'),
+
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.commonjs
+			},
+
+			ecmaVersion: 2019,
+			sourceType: 'commonjs'
+		},
+
+		rules: {
+			'no-console': 'off',
+			indent: ['error', 'tab'],
+			'linebreak-style': ['error', 'unix'],
+			quotes: ['error', 'single'],
+			semi: ['error', 'never']
+		}
+	}
+])
